58dedbcaab1073794ebab1ddd423dfd9a8e9f40c
changsman <90780580+changsman@users.noreply.github.com>
2021-09-16 00:06:27 +0900

    Delete ponketmon.py

--- ponketmon.py
+++
@@ -1,9 +0,0 @@
-def solution(nums):
-    nums_array=list(set(nums))
-    if len(nums_array)>len(nums)//2:
-            
-      answer = len(nums)//2
-      return answer
-    else:
-     answer=len(nums_array)
-     return answer
